d595fe8b35d8f1fed31a4bb4987a3e7075795663
David Arranz González <a25davidag@teis.intranet>
2026-01-28 15:53:52 +0100

    hacer las interfaces

--- src/modelo/contenido/Contenido.java
+++ src/modelo/contenido/Contenido.java
@@ -1,4 +1,107 @@
 package modelo.contenido;
 
-public class Contenido {
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public abstract class Contenido {
+
+    private String id;
+    private String titulo;
+    private int reproducciones;
+    private int likes;
+    private int duracionSegundos;
+    private ArrayList<String>tags;
+    private boolean disponible;
+    private LocalDate fechaPublicacion;
+
+    public Contenido(String id, LocalDate fechaPublicacion, boolean disponible, ArrayList<String> tags, int duracionSegundos, int likes, int reproducciones, String titulo) {
+        this.id = id;
+        this.fechaPublicacion = fechaPublicacion;
+        this.disponible = disponible;
+        this.tags = new ArrayList<>();
+        this.duracionSegundos = duracionSegundos;
+        this.likes = likes;
+        this.reproducciones = reproducciones;
+        this.titulo = titulo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(LocalDate fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void addTags(String tags) {
+        this.tags.add(tags);
+    }
+
+    public int getDuracionSegundos() {
+        return duracionSegundos;
+    }
+
+    public void setDuracionSegundos(int duracionSegundos) {
+        this.duracionSegundos = duracionSegundos;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getReproducciones() {
+        return reproducciones;
+    }
+
+    public void setReproducciones(int reproducciones) {
+        this.reproducciones = reproducciones;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    abstract void reproducir();
+
+    void aumentarReproducciones(){}
+
+    void agregarLike(){}
+
+    boolean esPopular(){
+        return false;
+    }
+
+    void validarDuracion(){}
+
+
+
 }
---
+++ src/modelo/contenido/Podcast.java
@@ -0,0 +1,91 @@
+package modelo.contenido;
+
+import enums.CategoriaPodcast;
+import modelo.artistas.Creador;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Podcast extends Contenido {
+
+    private Creador creador;
+    private int numeroEpisodio;
+    private int temporada;
+    private String descripcion;
+    private CategoriaPodcast categoriaPodcast;
+    private ArrayList<String>invitado;
+    private String transcripcion;
+
+
+    public Podcast(String id, LocalDate fechaPublicacion, boolean disponible, ArrayList<String> tags, int duracionSegundos, int likes, int reproducciones, String titulo, String transcripcion, CategoriaPodcast categoriaPodcast, int numeroEpisodio, Creador creador, int temporada, String descripcion, ArrayList<String> invitados) {
+        super(id, fechaPublicacion, disponible, tags, duracionSegundos, likes, reproducciones, titulo);
+        this.transcripcion = transcripcion;
+        this.categoriaPodcast = categoriaPodcast;
+        this.numeroEpisodio = numeroEpisodio;
+        this.creador = creador;
+        this.temporada = temporada;
+        this.descripcion = descripcion;
+        this.invitado = new ArrayList<>();
+    }
+
+    public Creador getCreador() {
+        return creador;
+    }
+
+    public void setCreador(Creador creador) {
+        this.creador = creador;
+    }
+
+    public ArrayList<String> getInvitados() {
+        return invitado;
+    }
+
+    public void setInvitados(String invitados) {
+        this.invitado.add(invitados);
+    }
+
+    public CategoriaPodcast getCategoriaPodcast() {
+        return categoriaPodcast;
+    }
+
+    public void setCategoriaPodcast(CategoriaPodcast categoriaPodcast) {
+        this.categoriaPodcast = categoriaPodcast;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(int temporada) {
+        this.temporada = temporada;
+    }
+
+    public int getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(int numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public String getTranscripcion() {
+        return transcripcion;
+    }
+
+    public void setTranscripcion(String transcripcion) {
+        this.transcripcion = transcripcion;
+    }
+
+    @Override
+    void reproducir() {
+
+    }
+}
